1a4c3f2ce4d7212cd74228f0af8d5c3a7a709c7a
ninjiacoder <rayzhao98@163.com>
2018-04-22 00:14:28 +0800

    20180421 update

--- README.md
+++ README.md
@@ -16,8 +16,11 @@ _需要安装 requests库_
 
  返回结果包含消费记录和充值距离 形式为JSON数组
 
-
-
+### 20180421 update
+1. 首先将JSON数据以可读形式输出，调用`getIn(data)`函数，可以输出在`data`所选日期范围内所有消费与充值记录，显示内容包括**地点、金额、消费类型**。
+2. 其次输出所选日期范围内的消费与支出金额，分别调用`getSpendMonthly(data)`函数与`getChargeMonthly(data)`函数。
+3. 为了之后使用Echart进行可视化，前期完成了根据不同年份，输入该年每月的消费与支出金额并保留一位小数。
+4. 优化代码，修改`begin_data`与`end_data`为`begin_date`与`end_date`;防止在一个py文件中多次请求出现错误的问题，设置了`isLogin`变量。
 
 ## Step 2：清洗数据，挖掘有效信息
 ## Step 3：搭建Web服务，用Echart做前端对数据进行可视化优化
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'arcosx'
+__author2__ = 'rayzhao98'
 
 import requests
+import threading
 
 
 class client(object):
     username = ""
     password = ""
+    isLogin = 0
     login_url = "https://ucapp.nuaa.edu.cn/wap/login/invalid"
     search_url = "https://app.nuaa.edu.cn/cardhis/wap/default/index"
     cookie = ""
@@ -21,6 +24,7 @@ class client(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.login()
 
     def login(self):
         post_data = {'username': self.username, 'password': self.password}
@@ -32,16 +36,76 @@ class client(object):
         vt = 'vt=' + cookies['vt']
         header_cookie = UUkey + vjuid + vjvd + vt
         self.headers['Cookie'] = header_cookie
+        self.isLogin = 1
         return 1
 
-    def search(self, begin_data, end_data):
-        if self.login() == 0:
+    def search(self, begin_date, end_date):
+        if self.isLogin == 0:
             return
-        payload = "sdate=" + begin_data + "&edate=" + end_data
+        payload = "sdate=" + begin_date + "&edate=" + end_date
         res = requests.post(self.search_url, headers=self.headers, data=payload)
         return res.json()
 
+def getChargeMonthly(data):
+    charge = 0
+    for date in data:
+        for item in data[date]:
+            if item['type'] == "充值":
+                charge += float(item["xfje"])
+    return charge
+    
+def getSpendMonthly(data):
+    spend = 0
+    for date in data:
+        for item in data[date]:
+            if item['type'] == "消费":
+                spend += float(item["xfje"])
+    return spend    
+
+def getIn(data):
+        for date in data:
+            print(date)
+            for item in data[date]:
+                print("在" + item['toname'] + item['type'] + "了" + item['xfje'] + '元')
+
+def getDaysInMonth(month):
+    return {
+        '1':  '31',
+        '2':  '28',
+        '3':  '31',
+        '4':  '30',
+        '5':  '31',
+        '6':  '30',
+        '7':  '31',
+        '8':  '31',
+        '9':  '30',
+        '10': '31',
+        '11': '30',
+        '12': '31',
+    }.get(month, 'error')
+
+def getData(begin_date, end_date):
+    data = test.search(begin_date=begin_date, end_date=end_date)['d']
+    print(begin_date[5:7])
+    print(round(getChargeMonthly(data), 2))
+    print(round(getSpendMonthly(data), 2))
 
 if __name__ == '__main__':
-    test = client(username='', password='')
-    print(test.search(begin_data='2015-06-06', end_data='2018-04-03'))
+    
+    test = client(username='161610336', password='133555')
+    date = "2017-"
+    for month in range(1, 13):
+        if month < 10:
+            monthString = '0' + str(month)
+        else:
+            monthString = str(month)
+        begin_date = date + monthString + '-01' 
+        end_date = date + monthString + '-' + getDaysInMonth(str(month))
+        t = threading.Thread(target=getData(begin_date, end_date), name="getDataThread")
+        t.start()
+        t.join()
+        
+    # print(test.search(begin_date='2018-04-01', end_date='2018-04-03')['d'].count)
+    # getIn(test.search(begin_date='2018-04-01', end_date='2018-04-03')['d'])
+    
+    
