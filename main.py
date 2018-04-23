3948cdb709e38e61c8a8760172562aa35bf1ffd6
ninjiacoder <rayzhao98@163.com>
2018-04-23 15:37:39 +0800

    20180423 update

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ __author2__ = 'rayzhao98'
 
 import requests
 import threading
-
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
 
 class client(object):
     username = ""
@@ -66,7 +68,9 @@ def getIn(data):
         for date in data:
             print(date)
             for item in data[date]:
-                print("在" + item['toname'] + item['type'] + "了" + item['xfje'] + '元')
+                # print("在" + item['toname'] + item['type'] + "了" + item['xfje'] + '元')
+                for index in item:
+                    print(index)
 
 def getDaysInMonth(month):
     return {
@@ -90,22 +94,84 @@ def getData(begin_date, end_date):
     print(round(getChargeMonthly(data), 2))
     print(round(getSpendMonthly(data), 2))
 
+def jsonToPanda(data):
+    columns = []
+    indexs = []
+    datas = []
+    dataOneDay = []
+    
+    for date in data:
+        for item in data[date]:
+            indexs.append(date)
+            columns = []
+            columns.append('date')
+            dataOneDay.append(date)
+            for index in item:
+                columns.append(index)
+                if index == 'xfje':
+                    dataOneDay.append(float(item[index]))
+                else:
+                    dataOneDay.append(item[index])
+            datas.append(dataOneDay)
+            dataOneDay = []
+
+    df = pd.DataFrame(datas, columns=columns)
+
+    print(df)
+    df.plot(x="date", y="xfje")
+    plt.show()
+
+def get2017SpeenMonthly(test):
+    begin_date = '2017-01-01'
+    end_date = '2017-12-31'
+    data = test.search(begin_date=begin_date, end_date=end_date)['d']
+
+    indexs = []
+    datas = []
+    dataMonthly = []
+
+    spendDaily = 0
+    spendMonthly = 0
+    currentMonth = 12
+
+    columns = ['month', 'spend']
+    # indexs.append(currentMonth)
+    
+    for date in data:
+
+        if int(date[5:7]) != currentMonth:
+            currentMonth -= 1
+            indexs.append(currentMonth+1)
+            dataMonthly.append(currentMonth+1)
+            dataMonthly.append(spendMonthly)
+            datas.append(dataMonthly)
+            dataMonthly = []
+            spendMonthly = 0
+        
+        for item in data[date]:
+            if item['type'] == '消费':
+                spendMonthly += float(item['xfje'])
+
+    indexs.append(currentMonth)
+    dataMonthly.append(currentMonth)
+    dataMonthly.append(spendMonthly)
+    datas.append(dataMonthly)
+    df = pd.DataFrame(datas, columns=columns)
+    print(df)
+    df.plot(kind="bar", title="Your spend monthly in 2017", x="month", y="spend")
+    for item in datas:
+        print(item)
+        plt.text(12-item[0], item[1]+0.05, '%.0f' % item[1], ha='center', va= 'bottom',fontsize=7)
+    plt.show()
+
 if __name__ == '__main__':
     
-    test = client(username='161610336', password='133555')
-    date = "2017-"
-    for month in range(1, 13):
-        if month < 10:
-            monthString = '0' + str(month)
-        else:
-            monthString = str(month)
-        begin_date = date + monthString + '-01' 
-        end_date = date + monthString + '-' + getDaysInMonth(str(month))
-        t = threading.Thread(target=getData(begin_date, end_date), name="getDataThread")
-        t.start()
-        t.join()
-        
-    # print(test.search(begin_date='2018-04-01', end_date='2018-04-03')['d'].count)
-    # getIn(test.search(begin_date='2018-04-01', end_date='2018-04-03')['d'])
+    test = client(username='', password='')
+    # begin_date = '2018-04-01'
+    # end_date = '2018-04-03'
+    # data = test.search(begin_date=begin_date, end_date=end_date)['d']
+    # jsonToPanda(data)
+
+    get2017SpeenMonthly(test)
     
     
